9ddea3bd24097f7b9229d70cbd47057cc89779ff
DeFUCC <fond@fondfrukt.ru>
2020-10-02 16:06:43 +0300

    centered bass

--- js/layers/bottom.js
+++ js/layers/bottom.js
@@ -4,30 +4,28 @@ export const bottom = {
   data() {
     return {
       layer: new paper.Layer({
-        name:'bottom'
+        name: 'bottom',
       }),
-      circles:[]
+      circles: [],
     }
   },
-  watch: {
-
-  },
+  watch: {},
   methods: {
     createBottom() {
-      for (let i=0;i<12;i++) {
+      for (let i = 0; i < 12; i++) {
         this.circles[i] = new paper.Shape.Circle({
-          center:[0,0],
-          radius:paper.view.bounds.height*0.4,
-          opacity:0,
-          strokeWidth:80,
-          strokeColor:{
-            hue:i*30,
-            lightness:0.5,
-            saturation:0.8
-          }
+          center: [0, 0],
+          radius: paper.view.bounds.height * 0.4,
+          opacity: 0,
+          strokeWidth: 80,
+          strokeColor: {
+            hue: i * 30,
+            lightness: 0.5,
+            saturation: 0.8,
+          },
         })
       }
-      this.positionCircle();
+      this.positionCircle()
     },
     positionCircle() {
       var width = paper.view.bounds.width,
@@ -35,49 +33,55 @@ export const bottom = {
         angle,
         x,
         y,
-        i;
+        i
 
       for (i = 0; i < 12; i++) {
-        angle = ((i-3) / (12 / 2)) * Math.PI; // Calculate the angle at which the element will be placed.
+        angle = ((i - 3) / (12 / 2)) * Math.PI // Calculate the angle at which the element will be placed.
         // For a semicircle, we would use (i / numNodes) * Math.PI.
-        x = ((width/3) * Math.cos(angle)) + (width / 2); // Calculate the x position of the element.
-        y = ((width/3) * Math.sin(angle)) + (width / 2); // Calculate the y position of the element.
-        this.circles[i].position=[x,y]
+        x = (width / 4) * Math.cos(angle) + width / 2 // Calculate the x position of the element.
+        y = (width / 4) * Math.sin(angle) + width / 4 // Calculate the y position of the element.
+        this.circles[i].position = [x, y]
       }
     },
     playNote(note) {
       if (this.circles[note.digit].t) {
-        this.circles[note.digit].t.stop();
+        this.circles[note.digit].t.stop()
       }
-      this.circles[note.digit].bringToFront();
-      this.circles[note.digit].tween({
-        opacity:1,
-      }, {
-        duration:100,
-        easing:'easeOutQuad'
-      })
+      this.circles[note.digit].bringToFront()
+      this.circles[note.digit].tween(
+        {
+          opacity: 1,
+        },
+        {
+          duration: 100,
+          easing: 'easeOutQuad',
+        },
+      )
     },
     stopNote(note) {
-       this.circles[note.digit].t = this.circles[note.digit].tween({
-        opacity:0,
-    //    radius:0
-      }, {
-        duration:3000,
-        easing:'easeInOutQuad'
-      })
-    }
+      this.circles[note.digit].t = this.circles[note.digit].tween(
+        {
+          opacity: 0,
+          //    radius:0
+        },
+        {
+          duration: 3000,
+          easing: 'easeInOutQuad',
+        },
+      )
+    },
   },
   mounted() {
-    this.$midiBus.$on('noteinon'+this.channel.num, this.playNote)
-    this.$midiBus.$on('noteinoff'+this.channel.num, this.stopNote)
+    this.$midiBus.$on('noteinon' + this.channel.num, this.playNote)
+    this.$midiBus.$on('noteinoff' + this.channel.num, this.stopNote)
     this.createBottom()
     window.addEventListener('resize', () => {
-      this.positionCircle();
+      this.positionCircle()
     })
   },
   beforeDestroy() {
-      this.$midiBus.$off('noteinon'+this.channel.num)
-      this.$midiBus.$off('noteinoff'+this.channel.num)
-      this.circles=undefined;
-  }
+    this.$midiBus.$off('noteinon' + this.channel.num)
+    this.$midiBus.$off('noteinoff' + this.channel.num)
+    this.circles = undefined
+  },
 }
